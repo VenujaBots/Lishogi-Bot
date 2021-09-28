0534172893ff5db9c04331e52a77095aeb7befd6
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2021-09-28 17:41:37 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,29 +1,11 @@
 import re
 
-# usi, uci
+# USI
 
 def isusi(move):
-    return bool(re.match("([1-9][A-i][1-9][A-i]\+?$)|([PLNSGBR]\*[1-9][A-i]$)", move))
+    return bool(re.match("([1-9][a-i][1-9][a-i]\+?$)|([PLNSGBR]\*[1-9][a-i]$)", move))
 
-def isuci(move):
-    return bool(re.match("([A-i][1-9][A-i][1-9]\+?$)|([PLNSGBR]\*[A-i][1-9]$)", move))
+# Makes sure the move is USI
 
-# changes uci to usi and usi to uci
-def switchusiuci(move) -> str:
-	transtable = {97: 57, 98: 56, 99: 55, 100: 54, 101: 53, 102: 52, 103: 51, 104: 50, 105: 49 }
-	transtable.update({v: k for k, v in transtable.items()})
-	return move.translate(transtable)
-
-# makes sure the move is usi
 def makeusi(move) -> str:
-    if(isuci(move)):
-        return switchusiuci(move)
-    else:
-        return move
-
-# makes sure the move is uci
-def makeuci(move) -> str:
-    if(isusi(move)):
-        return switchusiuci(move)
-    else:
-        return move
+    return move
